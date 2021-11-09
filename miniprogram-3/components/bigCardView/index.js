fd053d7a4690d03d3768d25349160cba4b846db2
UmyNya <1079102275@qq.com>
2021-11-09 16:25:41 +0800

    update the style of the home page and the several card views. Besides, I add some style libararies: iView (./dist/..) and Vant (./distVant/..)

---
+++ miniprogram-3/components/bigCardView/index.js
@@ -0,0 +1,24 @@
+Component({
+    options: {
+        multipleSlots: true
+    },
+
+    properties: {
+        full: {
+            type: Boolean,
+            value: false
+        },
+        thumb: {
+            type: String,
+            value: ''
+        },
+        title: {
+            type: String,
+            value: ''
+        },
+        extra: {
+            type: String,
+            value: ''
+        }
+    }
+});
--- miniprogram-3/pages/index/index.js
+++ miniprogram-3/pages/index/index.js
@@ -6,21 +6,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    goods: [],
+    RequestViewImg:['../../icon/view0.png','../../icon/view1.png'], //委托的视图的图标列表
+    TradeViewImg:['../../icon/view0.png','../../icon/view2.png'], //交易的视图的图标列表
+    viewType:true, //视图种类 true表示带图，false表示无图
+    requestList:[] //委托列表
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    console.log('onLoad');
+    util.request('http://localhost:1314/goodsPage')
+        .then(res => {
+            console.log(res)
+            this.setData({
+                goods: res.data.goods
+            })
+        })  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    console.log('onPullDownRefresh');
+    wx.showToast({
+        title: '刷新完成',
+        icon: 'success',
+        duration: 2000
+    })
   },
 
   /**
