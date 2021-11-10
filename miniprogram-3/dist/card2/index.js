4f32dfec378217b508ebe9b5fd3c4307b1214414
UmyNya <1079102275@qq.com>
2021-11-10 18:54:39 +0800

    修改了卡片视图的样式和视图切换标签

--- miniprogram-3/dist/card2/index.js
+++ miniprogram-3/dist/card2/index.js
@@ -21,6 +21,29 @@ Component({
         extra: {
             type: String,
             value: ''
+        },
+        price: {
+            type: String,
+            value: '',
+            observer: 'updatePrice',
+        },
+        other:{
+            type: String,
+            value: ''
         }
+    },
+
+    methods: {
+        updatePrice() {
+            const { price } = this.data;
+            const priceArr = price.toString().split('.');
+            this.setData({
+                integerStr: priceArr[0],
+                decimalStr: priceArr[1] ? `.${priceArr[1]}` : '',
+            });
+        },
+        onClickThumb() {
+            this.jumpLink('thumbLink');
+        },
     }
 });
