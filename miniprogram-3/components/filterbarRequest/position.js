846952400387d5e8434ed94935c5aaaabc2bb8ad
UmyNya <1079102275@qq.com>
2021-11-12 23:28:46 +0800

    调整了分类页面的筛选栏

--- miniprogram-3/components/filterbarRequest/position.js
+++ miniprogram-3/components/filterbarRequest/position.js
@@ -1,41 +1,5 @@
-const typeData = {
-  value: '2',
-  label: '类型',
-  children: [{
-    value: '0',
-    label: '不限'
-  }, {
-    value: '1',
-    label: '委托'
-  }, {
-    value: '2',
-    label: '问答'
-  }, {
-    value: '3',
-    label: '交易'
-  }]
-}
-
-const radioData = {
-  value: '2',
-  label: '类型',
-  children: [{
-    value: '0',
-    label: '不限'
-  }, {
-    value: '1',
-    label: '委托'
-  }, {
-    value: '2',
-    label: '问答'
-  }, {
-    value: '3',
-    label: '交易'
-  }]
-}
-
 const sortData = {
-  value: '3',
+  value: '0',
   label: '排序',
   children: [{
     value: '0',
@@ -52,109 +16,75 @@ const sortData = {
   }]
 }
 
-const multiData = {
-  label: '筛选',
+const taskData = {
+  value: '1',
+  label: '委托类别',
   children: [{
-      value: '4',
-      label: '学历',
-      children: [{
-          value: '0',
-          label: '不限'
-        }, {
-          value: '1',
-          label: '高中及以下'
-        }, {
-          value: '2',
-          label: '大专'
-        },
-        {
-          value: '3',
-          label: '本科'
-        }, {
-          value: '4',
-          label: '硕士'
-        }, {
-          value: '5',
-          label: '博士'
-        }
-      ]
-    },
-    {
-      value: '5',
-      label: '规模',
-      children: [{
-          value: '0',
-          label: '不限',
-        }, {
-          value: '1',
-          label: '0-50人'
-        }, {
-          value: '2',
-          label: '50-100人'
-        },
-        {
-          value: '3',
-          label: '100-500人'
-        }, {
-          value: '4',
-          label: '500-1000人'
-        }, {
-          value: '5',
-          label: '1000人以上'
-        }
-      ]
-    },
-    {
-      value: '6',
-      label: '薪资',
-      children: [{
-        value: '0',
-        label: '不限'
-      }, {
-        value: '1',
-        label: '5k以下'
-      }, {
-        value: '2',
-        label: '5K~10K'
-      }, {
-        value: '3',
-        label: '10K以上'
-      }]
-    },
-    {
-      value: '7',
-      label: '工作经验',
-      children: [{
-          value: '0',
-          label: '不限'
-        }, {
-          value: '1',
-          label: '应届'
-        }, {
-          value: '2',
-          label: '1年以下'
-        },
-        {
-          value: '3',
-          label: '1-3年'
-        }, {
-          value: '4',
-          label: '3-5年'
-        }, {
-          value: '5',
-          label: '5-10年'
-        }, {
-          value: '6',
-          label: '10年以上'
-        }
-      ]
-    }
-  ]
+    value: '0',
+    label: '不限'
+  }, {
+    value: '1',
+    label: '外卖代拿'
+  }, {
+    value: '2',
+    label: '快递代拿'
+  }, {
+    value: '3',
+    label: '其他'
+  }]
+}
+
+const questionData = {
+  value: '2',
+  label: '问答类别',
+  children: [{
+    value: '0',
+    label: '不限'
+  }, {
+    value: '1',
+    label: '学习'
+  }, {
+    value: '2',
+    label: '生活'
+  }, {
+    value: '3',
+    label: '讲座'
+  }, {
+    value: '4',
+    label: '比赛'
+  }, {
+    value: '5',
+    label: '实习'
+  }]
+}
+
+const tradeData = {
+  value: '3',
+  label: '交易类别',
+  children: [{
+    value: '0',
+    label: '不限'
+  }, {
+    value: '1',
+    label: '书籍'
+  }, {
+    value: '2',
+    label: '食品'
+  }, {
+    value: '3',
+    label: '日用品'
+  }, {
+    value: '4',
+    label: '电子产品'
+  }, {
+    value: '3',
+    label: '其他'
+  }]
 }
 
 module.exports = {
-  typeData: typeData,
-  radioData: radioData,
   sortData: sortData,
-  multiData: multiData
+  taskData: taskData,
+  questionData: questionData,
+  tradeData: tradeData
 }
